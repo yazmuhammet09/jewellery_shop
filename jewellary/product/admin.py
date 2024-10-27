9b7c3c22ff1b05a84d176cebcacc6b2ad1bb1831
yazmuhammet1 <rlax1113@gmail.com>
2024-10-27 02:23:02 -0700

    yazmuhammet

--- jewellary/product/admin.py
+++ jewellary/product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import About
 
-# Register your models here.
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('address', 'number', 'email', 'description', 'work_time', 'social_network')
+
+admin.site.register(About, AboutAdmin)
--- jewellary/product/models.py
+++ jewellary/product/models.py
@@ -4,7 +4,7 @@ class About(models.Model):
     address = models.CharField(max_length=255)
     number = models.IntegerField()
     email = models.CharField(max_length=255)
-    descriptoin = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     work_time = models.IntegerField()
     social_network = models.CharField(max_length=255)
 
