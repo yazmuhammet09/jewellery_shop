6bc6e949fd2118378cb88ff1fa963231c3607caa
Ilyas <ilyastachmammedov@gmail.com>
2024-10-27 13:35:25 +0500

    product commited

--- jewellary/product/admin.py
+++ jewellary/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import product, material, category
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'amount', 'weight', 'color')
+
+admin.site.register(product)
 
 # Register your models here.
