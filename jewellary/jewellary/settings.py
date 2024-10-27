c43d380a4f544a9417e177679fac3f67f646b700
yazmuhammet1 <rlax1113@gmail.com>
2024-10-27 01:32:59 -0700

    added model About

--- jewellary/jewellary/settings.py
+++ jewellary/jewellary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#4up7tx@!=zy%+r_bj*$0(&t1z7k_m_i1wmgk4=y)4lvs$(q9v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product'
 ]
 
 MIDDLEWARE = [
--- jewellary/product/models.py
+++ jewellary/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class About(models.Model):
+    address = models.CharField(max_length=255)
+    number = models.IntegerField()
+    email = models.CharField(max_length=255)
+    descriptoin = models.CharField(max_length=255)
+    work_time = models.IntegerField()
+    social_network = models.CharField(max_length=255)
+
+
