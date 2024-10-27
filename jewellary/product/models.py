0c8cd48480784a327205b00ee533d524ca6f1509
shikimori-dot <aysenemturapova90@gmail.com>
2024-10-27 14:17:52 +0500

    added search

--- jewellary/product/models.py
+++ jewellary/product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    amount = models.IntegerField()
+    weight = models.IntegerField()
+    color = models.CharField(max_length=50)
+    gender = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} {self.price} {self.gender}"
+
+class material(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.name}"
--- jewellary/product/views.py
+++ jewellary/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
-# Create your views here.
+def search(request):
+    filter_ = Product.objects.filter(field_name='name')
+    query = request.GET.get('q')
+    if query : 
+        product = Product.objects.filter(field_name_icontains=query)
+    else:
+        product=[]
+    context = {'data':product}
+    return(request, "template.html", context)
